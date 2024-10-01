2ec6605f6804332c7f8a5a458220948e72c80ceb
Iwirii <ksu0655@mail.ru>
2024-10-01 18:56:07 +0600

    Create Задание 3.cs

---
+++ Алгоритмизация и программирование/1 семестр/26.09.24/Задание 3.cs
@@ -0,0 +1,29 @@
+//Необходимо определить 2 максимума в последовательности
+using System;
+class Test
+{
+    static void Main()
+    {
+        int n, a, max, max2;
+        Console.WriteLine("Введите количество чисел в последовательности:");
+        n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число:");
+        a = Convert.ToInt32(Console.ReadLine());
+        max = max2 = a;
+        for (int i = 1; i < n; i++)
+        {
+            Console.WriteLine("Введите число:");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a > max)
+            {
+                max2 = max;
+                max = a;
+            }
+            else
+            if ( a < max && ( a > max2 || max2 == max ) )
+                    max2 = a;
+    }
+        Console.WriteLine(max);
+        Console.WriteLine(max2);
+    }
+}
