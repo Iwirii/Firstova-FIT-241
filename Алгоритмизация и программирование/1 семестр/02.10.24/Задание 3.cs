d26baca5544163763fb42089a4358c957569a7c9
Iwirii <ksu0655@mail.ru>
2024-10-10 10:28:32 +0600

    Create Задание 3.cs

---
+++ Алгоритмизация и программирование/1 семестр/02.10.24/Задание 3.cs
@@ -0,0 +1,28 @@
+using System;
+public class Test
+{
+	public static void Main()
+	{
+	    int a,sum=0;
+	    int n = Convert.ToInt32(Console.ReadLine());
+        int maxi=0;
+        for (int i=0;i<n;i++)
+        {
+            a = Convert.ToInt32(Console.ReadLine());  
+            if (a%2==0)
+            {
+                sum+=a;
+            }
+            else
+            {
+                if (sum>0) 
+                {
+                    maxi=Math.Max(sum,maxi);
+                }
+            sum=0;
+            }
+        }
+        maxi=Math.Max(sum,maxi);
+        Console.WriteLine(maxi);
+    }
+}
