b58ee0b53170a224e4706e8313dd100354fc801c
Iwirii <ksu0655@mail.ru>
2024-10-10 10:08:43 +0600

    Create Задание 2.cs

---
+++ Алгоритмизация и программирование/1 семестр/02.10.24/Задание 2.cs
@@ -0,0 +1,38 @@
+using System;
+public class Test
+{
+	public static void Main()
+	{
+	    int a,cnt=0;
+	    int n = Convert.ToInt32(Console.ReadLine());
+        int mini=n+1;
+        for (int i=0;i<n;i++)
+        {
+            a = Convert.ToInt32(Console.ReadLine());  
+            if (a%2==0)
+            {
+                cnt++;
+            }
+            else
+            {
+                if (cnt>0) 
+                {
+                    mini=Math.Min(cnt,mini);
+                }
+            cnt=0;
+            }
+        }
+        if (cnt>0) 
+        {
+            mini=Math.Min(cnt,mini);
+        }
+        if (mini == n+1)
+        {
+            Console.WriteLine("Чётных элементов нет");
+        }
+        else
+        {
+            Console.WriteLine(mini);
+        }
+    }
+}
