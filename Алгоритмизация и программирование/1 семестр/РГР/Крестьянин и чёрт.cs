6dc1ecb8967fa1d876c070642e91ae8de719557f
Iwirii <ksu0655@mail.ru>
2024-12-06 12:54:53 +0600

    Create Крестьянин и чёрт.cs

---
+++ Алгоритмизация и программирование/1 семестр/РГР/Крестьянин и чёрт.cs
@@ -0,0 +1,17 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите целое число (n) - максимальное количество, которое может быть изначально у крестьянина:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int res = n;
+        for (int k = 3; k <= n; k = 2 * k + 1)
+        {
+            int z = n / k;
+            res += z;
+        }
+
+        Console.WriteLine(res);
+    }
+}
