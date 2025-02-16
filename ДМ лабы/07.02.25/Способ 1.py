d1930038fc576c9c2b4c7a1f97a330fb340fc4c1
Iwirii <ksu0655@mail.ru>
2025-02-16 17:00:03 +0600

    Update Способ 1.py

--- ДМ лабы/07.02.25/Способ 1.py
+++ ДМ лабы/07.02.25/Способ 1.py
@@ -11,7 +11,7 @@ for i in range(cnt_vershin):
         komponenta = []
         queue = [i]
         visited[i] = True
-        while len(queue)>0:
+        while len(queue) > 0:
             current_vertex = queue.pop(0)
             komponenta.append(current_vertex + 1)
             for j in range(cnt_vershin):
